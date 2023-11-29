00aa322acaa6f6a67bf0d745ac03445d467a9932
Samuel <s.looi2@outlook.com>
2023-11-29 18:12:32 +1300

    change response

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-	res.send("<h1>azureDeploy2 working!</h1>");
+	res.send("<h1>azureDeploy2 working! trying tsx again</h1>");
 });
 
 app.listen(PORT, () => console.log("LISTENING ON PORT " + PORT));
