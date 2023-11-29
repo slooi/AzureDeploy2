978e1e87b0f542fcfdffefc3e037a77832de1ff5
Samuel <s.looi2@outlook.com>
2023-11-29 17:39:12 +1300

    change port

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
 	res.send("<h1>azureDeploy2 working!</h1>");
